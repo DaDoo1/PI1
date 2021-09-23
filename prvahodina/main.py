627ae5f0f7f0988e0271571a7763a0944ec8a98c
z21ite12 <z21ite12@z00p3-06.SPSKNM>
2021-09-23 14:46:10 +0200

    zaklady

--- prvahodina/main.py
+++ prvahodina/main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+print("ahoj svet")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x = 5
+vysledok = x * x
+vysledok = x * 4
+print("obsah x*x:" ,vysledok)
+print(o)
