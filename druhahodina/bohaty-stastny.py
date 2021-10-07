968710e671be92e4203c995bc004c2f2d579d8c8
DaDoo1 <91258437+DaDoo1@users.noreply.github.com>
2021-10-07 14:11:08 +0200

    update uloha

---
+++ druhahodina/bohaty-stastny.py
@@ -0,0 +1,16 @@
+otazkasiStastny = input("si stastny ? ")
+otazkasiBohaty = input("si bohaty ? ")
+
+if(otazkasiStastny == "ano"):
+    if(otazkasiBohaty == "ano"):
+        print("gratulujem")
+    else:
+        print("skus viac setrit")
+else:
+    if(otazkasiBohaty == "ano"):
+        print("skus sa viac usmievat")
+    else:
+        print("sorry je mi to luto")
+
+
+
