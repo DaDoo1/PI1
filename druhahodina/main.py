d4e2822567c557178b69a3c47518bf59dd8a5f6c
DaDoo1 <91258437+DaDoo1@users.noreply.github.com>
2021-10-07 13:54:42 +0200

    update 2 hodina

---
+++ druhahodina/main.py
@@ -0,0 +1,75 @@
+#vek = int(input("zadaj svoj vek: "))
+#print(vek + 10)
+
+#polomerKruhu = float(input("zadaj polomer kruhu: "))
+#obvodkruhu = 2*3.14*polomerKruhu
+#print("obvod kruhu je", obvodkruhu)
+
+"""
+a = int(input("zadaj a: "))
+b = int(input("zadaj b: "))
+print("scitanie:", a + b)
+print("odcitanie:", a - b)
+print("nasobenie:", a * b)
+if b == 0:
+    print("nulou sa delit neda")
+else:
+    print("delenie:", a / b)
+"""
+
+
+#+-, *, %, **, /                - aritmeticke operatory
+#==, !=, <, >, <=, >=           - logicke operatory
+#vysledok = 1 != 2
+#print(vysledok)
+
+"""
+vek = int(input("zadaj svoj vek: "))
+if vek >= 18:
+    print("mas dost rokov, vstup dalej")
+    print("vitaj medzi nami")
+else:
+    print("vstup zakazany, si moc mlady")
+print("tento kod sa vykona vzdy")
+"""
+
+"""
+day = 5
+hour = 12
+if day == 1:
+    print("pondelok")
+elif day == 2:
+    print("utorok")
+elif day == 3:
+    print("streda")
+elif day == 4:
+    print("stvrtok")
+elif day == 5:
+    print("piatok")
+    if hour < 12:
+        print("je doobeda")
+    else:
+        print("je poobede")
+elif day == 6:
+    print("sobota")
+elif day == 7:
+    print("nedela")
+"""
+
+""""
+username = input ("zadaj prihlasovacie meno: ")
+password = input ("zadaj prihlasovacie heslo: ")
+vek = input("zadaj svoj vek: ")
+if username == "root" and password == "password" and vek == "18":
+    print("uspesne si sa prihlasil!")
+else:
+    print("zle meno alebo heslo alebo vek")
+"""
+
+
+
+
+
+
+
+
