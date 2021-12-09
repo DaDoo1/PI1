cce9bd7bc21cc6417213f5f8acb4b32b37f1194c
DaDoo1 <91258437+DaDoo1@users.noreply.github.com>
2021-12-09 14:29:50 +0100

    sach

---
+++ pythonProject1/main.py
@@ -0,0 +1,43 @@
+"""
+cisla = [4, 5, 6, 7]
+print(cisla[3])
+
+
+pole = [
+    [1,2,3]
+    [4,5,6]
+    [7,8,9]
+]
+
+print(pole[1][1])
+pole[0].append(99)
+"""
+
+"""
+def vypisKinosalu(kinosala):
+    for riadok in kinosala:
+        print(riadok)
+
+
+
+kinosala =[]
+
+for i in range(5):
+    temp = []
+    for i in range(10):
+        temp.append(0)
+    kinosala.append(temp)
+
+
+print(kinosala)
+
+
+vypisKinosalu(kinosala)
+print()
+kinosala[0][5] = 1
+kinosala[2][4] = 1
+kinosala[1][3] = 1
+vypisKinosalu(kinosala)
+"""
+
+
